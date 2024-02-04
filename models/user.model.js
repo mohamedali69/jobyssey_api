d75e698e325d68eef232cab3c2ef980f8ce949ee
mohamedali69 <dalichakhari6801@gmail.com>
2024-02-04 14:28:08 +0100

    update user model

--- models/user.model.js
+++ models/user.model.js
@@ -29,6 +29,10 @@ module.exports = (sequelize, Sequelize) => {
       type: Sequelize.STRING,
       allowNull: false,
     },
+    paysCode:{
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
     telephone: {
       type: Sequelize.STRING,
       allowNull: false,
